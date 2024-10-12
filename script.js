799406b2cfcef1ce4c30a991230ca4957f6089fe
Rizzz Maooo <rizqonmaulana05@gmail.com>
2024-10-12 20:26:39 +0700

    update

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// const clientWidth = window.innerWidth
-// const html = document.querySelector('html')
-// const rootFontSize = Math.floor(clientWidth / 80)
-// html.style.fontSize = rootFontSize.toString() + 'px'
-
-// const navigasi = document.getElementById('navigasi')
-// navigasi.style.width = (0.2 * clientWidth).toString() + 'px'
-
-// const container = document.getElementById('container')
-// container.style.width = (0.8 * clientWidth).toString() + 'px'
-// container.style.marginLeft = navigasi.style.width
 
 const preloader = document.getElementById('preloader')
 const container = document.getElementById('container')
@@ -17,3 +6,15 @@ window.addEventListener('load', function() {
   container.style.display = 'flex'
   preloader.style.display = 'none'
 })
+
+const clientWidth = window.innerWidth
+const html = document.querySelector('html')
+const rootFontSize = Math.floor(clientWidth / 80)
+html.style.fontSize = rootFontSize.toString() + 'px'
+
+// const navigasi = document.getElementById('navigasi')
+// navigasi.style.width = (0.2 * clientWidth).toString() + 'px'
+
+// const container = document.getElementById('container')
+// container.style.width = (0.8 * clientWidth).toString() + 'px'
+// container.style.marginLeft = navigasi.style.width
