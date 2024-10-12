9d4e8a9764ad8e69c74307052b0a0d7617157e15
Rizzz Maooo <rizqonmaulana05@gmail.com>
2024-10-12 14:12:41 +0700

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+// const clientWidth = window.innerWidth
+// const html = document.querySelector('html')
+// const rootFontSize = Math.floor(clientWidth / 80)
+// html.style.fontSize = rootFontSize.toString() + 'px'
+
+// const navigasi = document.getElementById('navigasi')
+// navigasi.style.width = (0.2 * clientWidth).toString() + 'px'
+
+// const container = document.getElementById('container')
+// container.style.width = (0.8 * clientWidth).toString() + 'px'
+// container.style.marginLeft = navigasi.style.width
+
+const preloader = document.getElementById('preloader')
+const container = document.getElementById('container')
+
+window.addEventListener('load', function() {
+  container.style.display = 'block'
+  preloader.style.display = 'none'
+})
